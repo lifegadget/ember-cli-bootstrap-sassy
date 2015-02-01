106f035b722f48adec896f7de0170225fb00ace5
Ken Snyder <ken@ken.net>
2015-02-01 12:16:36 +0000

    small fixes, better status message

--- Brocfile.js
+++ Brocfile.js
@@ -5,14 +5,19 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 var app = new EmberAddon();
 var bootstrapPath   = 'bower_components/bootstrap-sass-official/assets/';
-var path = require('path');
+var path 			= require('path');
+var modulePath      = path.relative(app.project.root, __dirname);
+var path_join = function(){
+  // fix path with windows back slash with path_join
+  return path.join.apply(this, arguments).replace(/\\/g, '/');
+};
 
 app.options.sassOptions = app.options.sassOptions || {};
 app.options.sassOptions.includePaths = app.options.sassOptions.includePaths || [];
 
-app.options.sassOptions.includePaths.push(bootstrapPath + 'stylesheets');
-app.options.sassOptions.includePaths.push(bootstrapPath + 'stylesheets/bootstrap');
-app.options.sassOptions.includePaths.push(bootstrapPath + 'stylesheets/bootstrap/mixins');
+app.options.sassOptions.includePaths.push(path_join(modulePath,bootstrapPath, 'stylesheets'));
+app.options.sassOptions.includePaths.push(path_join(modulePath,bootstrapPath, 'stylesheets/bootstrap'));
+app.options.sassOptions.includePaths.push(path_join(modulePath,bootstrapPath, 'stylesheets/bootstrap/mixins'));
 
 app.import(bootstrapPath + 'javascripts/bootstrap.js');
 
--- index.js
+++ index.js
@@ -4,31 +4,39 @@ module.exports = {
 	included: function included(app) {
 		this.app = app;
 		var configMessage = []; 
-
+		var path 			= require('path');
+		var o         		= app.options['ember-cli-bootstrap-sassy'] || { js: true, glyphicons: true };
+		var bootstrapPath   = 'bower_components/bootstrap-sass-official/assets/';
+		var modulePath      = path.relative(app.project.root, __dirname);
+		var path_join = function(){
+		  // fix path with windows back slash with path_join
+		  return path.join.apply(this, arguments).replace(/\\/g, '/');
+		};
+		
 		var emberCLIVersion = app.project.emberCLIVersion().split(',').map(function(item) {return Number(item);});
 		if (emberCLIVersion[1] === 0  || emberCLIVersion[2] < 8) {
 			throw new Error('ember-cli-bootstrap-sassy requires ember-cli version 0.1.8 or greater.\n');
 		}
-
-		var o         		= app.options['ember-cli-bootstrap-sassy'] || {};
-		var bootstrapPath   = 'bower_components/bootstrap-sass-official/assets/';
     
 		// add paths to SASS install
-		console.log('Bootstrap-Sassy adding Bootstrap\'s path to ember-cli-sass\'s options');
 		app.options.sassOptions = app.options.sassOptions || {};
 		app.options.sassOptions.includePaths = app.options.sassOptions.includePaths || [];
 
-		app.options.sassOptions.includePaths.push(bootstrapPath + 'stylesheets');
-		app.options.sassOptions.includePaths.push(bootstrapPath + 'stylesheets/bootstrap');
-		app.options.sassOptions.includePaths.push(bootstrapPath + 'stylesheets/bootstrap/mixins');
+		app.options.sassOptions.includePaths.push(path_join(bootstrapPath, 'stylesheets'));
+		app.options.sassOptions.includePaths.push(path_join(bootstrapPath, 'stylesheets/bootstrap'));
+		app.options.sassOptions.includePaths.push(path_join(bootstrapPath, 'stylesheets/bootstrap/mixins'));
 
 		// Import JS from bootstrap
 		if(o.js instanceof Array) {
 			o.js.forEach(function(fileName) {
 				app.import(bootstrapPath + 'javascripts/bootstrap', fileName + '.js');
 			});
+			configMessage.push('some JS loaded [%@]'.fmt(o.js.join(',')));
 		} else if (o.js !== false) {
 			app.import(bootstrapPath + 'javascripts/bootstrap.js');
+			configMessage.push('all JS enabled');
+		} else {
+			configMessage.push('no JS enabled');
 		}
 		
 		// Import glyphicons from bootstrap
@@ -37,7 +45,13 @@ module.exports = {
 			app.import(bootstrapPath + 'fonts/bootstrap/glyphicons-halflings-regular.svg', { destDir: '/fonts/bootstrap' });
 			app.import(bootstrapPath + 'fonts/bootstrap/glyphicons-halflings-regular.ttf', { destDir: '/fonts/bootstrap' });
 			app.import(bootstrapPath + 'fonts/bootstrap/glyphicons-halflings-regular.woff', { destDir: '/fonts/bootstrap' });
+			configMessage.push('glphicons enabled');
+		} else {
+			configMessage.push('glphicons disabled');
 		}
+		
+		console.log('bootstrap-sassy config: ', configMessage.join(', '));
+		
 	}
 	
 };

a94e7fcec9c1396bae92ef164387d6efc4a5e12c
Ben Limmer <ben@benlimmer.com>
2017-03-29 14:36:17 -0700

    Update ember-cli. Also, resolve deprecation reported in #46.

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -4,28 +4,33 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dummy',
     environment: environment,
-    baseURL: '/',
+    rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
-      }
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
+      },
     },
 
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
     },
-	contentSecurityPolicy: {
-	    'default-src': "'unsafe-eval' http://cloudfront.net",
-	    'script-src': "'self'",
-	    'font-src': "'self' http://fonts.gstatic.com http://fonts.googleapis.com",
-	    'connect-src': "'self'",
-	    'img-src': "'self'",
-	    'style-src': "'self' 'unsafe-inline' http://fonts.googleapis.com",
-	    'media-src': "'self'"
-	  }
+
+    contentSecurityPolicy: {
+      'default-src': "'unsafe-eval' http://cloudfront.net",
+      'script-src': "'self'",
+      'font-src': "'self' http://fonts.gstatic.com http://fonts.googleapis.com",
+      'connect-src': "'self'",
+      'img-src': "'self'",
+      'style-src': "'self' 'unsafe-inline' http://fonts.googleapis.com",
+      'media-src': "'self'"
+    }
   };
 
   if (environment === 'development') {
@@ -38,7 +43,6 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
